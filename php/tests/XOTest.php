5e73fb19b0e0369ba9aff5259b013e566f262672
olivenbarcelon <olivenbarcelon@gmail.com>
2023-07-27 15:21:45 +0800

    add square every digit

--- php/tests/XOTest.php
+++ php/tests/XOTest.php
@@ -4,13 +4,13 @@ namespace Src;
 
 use PHPUnit\Framework\TestCase;
 
-final class XOTest extends TestCase {
+class XOTest extends TestCase {
     /**
      * @test
      * @testdox It should proceed exes and ohs
      * @return void
      */
-    public function exesAndOhs() {
+    public function exesAndOhs(): void {
         $this->assertSame(true, XO::exesAndOhs('ooxx'));
         $this->assertSame(false, XO::exesAndOhs('xooxx'));
         $this->assertSame(true, XO::exesAndOhs('ooxXm'));
