87f0bdc6f7d11d006a38803082d705ddff15b1eb
olivenbarcelon <olivenbarcelon@gmail.com>
2023-08-26 17:00:10 +0800

    add dig pow

---
+++ project/js/NumeralUtility.js
@@ -0,0 +1,33 @@
+"use strict";
+
+class NumeralUtility {
+    /**
+     * @title Playing with Digits
+     * @description
+     * Some numbers have funny properties. For example:
+     * 89 --> 8¹ + 9² = 89 * 1
+     * 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+     * 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+     * Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p we want to find a positive integer k, if it exists, such as the sum of the digits of n taken to the successive powers of p is equal to k * n. In other words:
+     * Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+     * If it is the case we will return k, if not return -1.
+     * Note: n, p will always be given as strictly positive integers.
+     * @version 0.0.1
+     * @author <olivenbarcelon@gmail.com>
+     * @param {int} n 
+     * @param {int} p 
+     * @returns {int}
+     */
+    static digPow = (n, p) => {
+        let input = [...n.toString()].map(Number);
+        let sumOfPow = 0;
+        for(let i = 0;i < input.length;i++) {
+            sumOfPow += Math.pow(input[i], i + p);
+        }
+        let k = sumOfPow / n;
+
+        return k >= 1 ? k : -1;
+    }
+}
+
+module.exports = NumeralUtility;
--- project/src/Digit.php
+++ project/src/Digit.php
@@ -19,29 +19,4 @@ class Digit {
         // Codewars Solution (kan6666)
         // return (int)implode(array_map(function($x){return $x * $x;}, str_split($num)));
     }
-
-    /**
-     * Title: Playing with Digits
-     * Description:
-     * Some numbers have funny properties. For example:
-     * 89 --> 8¹ + 9² = 89 * 1
-     * 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
-     * 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
-     * Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p we want to find a positive integer k, if it exists, such as the sum of the digits of n taken to the successive powers of p is equal to k * n. In other words:
-     * Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
-     * If it is the case we will return k, if not return -1.
-     * Note: n, p will always be given as strictly positive integers.
-     * @param integer $n
-     * @param integer $p
-     * @return integer
-     */
-    public static function digPow(int $n, int $p): int {
-        // My Solution
-        $array = str_split($n);
-        for($i = 0;$i < count($array);$i++) {
-            $pow[$i] = pow($array[$i], $i + $p);
-        }
-        $k = array_sum($pow) / $n;
-        return $k >= 1 ? $k : -1;
-    }
 }
