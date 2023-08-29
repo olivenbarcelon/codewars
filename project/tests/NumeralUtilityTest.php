bbe79d33154f498b86add00e5a8ef871621cf3c1
olivenbarcelon <olivenbarcelon@gmail.com>
2023-08-29 19:12:48 +0800

    add to roman

--- project/tests/NumeralUtilityTest.php
+++ project/tests/NumeralUtilityTest.php
@@ -15,18 +15,4 @@ class NumeralUtilityTest extends TestCase {
         $this->assertSame(16.5, NumeralUtility::meanSquare([10, 20, 10, 2], [10, 25, 5, -2]));
         $this->assertSame(1, NumeralUtility::meanSquare([-1, 0], [0, -1]));
     }
-
-    /**
-     * @test
-     * @testdox It should run to roman
-     * @return void
-     */
-    public function toRoman(): void {
-        $this->assertSame('MCMXC', NumeralUtility::toRoman(1990));
-        $this->assertSame('MMVIII', NumeralUtility::toRoman(2008));
-        $this->assertSame('MDCLXVI', NumeralUtility::toRoman(1666));
-        $this->assertSame("M", NumeralUtility::toRoman(1000));
-        $this->assertSame("IV", NumeralUtility::toRoman(4));
-        $this->assertSame("I", NumeralUtility::toRoman(1));
-    }
 }
