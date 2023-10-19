24364f4c2b6046cdf5a128d21ade34b6e36631b1
olivenbarcelon <olivenbarcelon@gmail.com>
2023-10-19 17:59:22 +0800

    add insert at

--- project/tests/dsa/ArrayList.test.js
+++ project/tests/dsa/ArrayList.test.js
@@ -8,7 +8,21 @@ beforeEach(() => {
 });
 
 describe("Array List", () => {
-    it("It should array list insert [12, 16, 20, 40, 50, 70, 26] to equal [12, 16, 20, 40, 50, 70, 26]", () => {
+    it("It should insert at, expect [2, 4, 1, 8, 5] to equal [2, 4, 10, 1, 8, 5]", () => {
+        array.insert(2);
+        array.insert(4);
+        array.insert(1);
+        array.insert(8);
+        array.insert(5);
+        array.insertAt(2, 10);
+        expect(array.getAll()).toEqual([2, 4, 10, 1, 8, 5]);
+    });
+
+    it("It should insert at throw an error", () => {
+        expect(() => array.insertAt(1, 1)).toThrow("Index 1 out of bounds for length 0");
+    });
+
+    it("It should insert [12, 16, 20, 40, 50, 70, 26] to equal [12, 16, 20, 40, 50, 70, 26]", () => {
         array.insert(12);
         array.insert(16);
         array.insert(20);
@@ -19,7 +33,7 @@ describe("Array List", () => {
         expect(array.getAll()).toEqual([12, 16, 20, 40, 50, 70, 26]);
     });
 
-    it("It should array list remove all, expect [3, 5, 8] to be []", () => {
+    it("It should remove all, expect [3, 5, 8] to be []", () => {
         array.add(3);
         array.add(5);
         array.add(8);
@@ -27,28 +41,28 @@ describe("Array List", () => {
         expect(array.getAll()).toEqual([]);
     });
 
-    it("It should array list index of 10 to be -1", () => {
+    it("It should index of 10 to be -1", () => {
         array.add(3);
         array.add(5);
         array.add(8);
         expect(array.indexOf(10)).toBe(-1);
     });
 
-    it("It should array list index of 5 to be 1", () => {
+    it("It should index of 5 to be 1", () => {
         array.add(3);
         array.add(5);
         array.add(8);
         expect(array.indexOf(5)).toBe(1);
     });
 
-    it("It should array list index of -1 to be -1", () => {
+    it("It should index of -1 to be -1", () => {
         array.add(3);
         array.add(5);
         array.add(8);
         expect(array.indexOf(-1)).toBe(-1);
     });
 
-    it("It should array list remove index, expect [3, 5, 8] to equal [3, 8]", () => {
+    it("It should remove index, expect [3, 5, 8] to equal [3, 8]", () => {
         array.add(3);
         array.add(5);
         array.add(8);
@@ -56,35 +70,35 @@ describe("Array List", () => {
         expect(array.getAll()).toEqual([3, 8]);
     });
 
-    it("It should array list get element throw error", () => {
+    it("It should get element throw error", () => {
         array.add(3);
         array.add(5);
         array.add(8);
         expect(() => array.get(3)).toThrow("Index 3 out of bounds for length 3");
     });
 
-    it("It should array list get element to be 5", () => {
+    it("It should get element to be 5", () => {
         array.add(3);
         array.add(5);
         array.add(8);
         expect(array.get(1)).toBe(5);
     });
 
-    it("It should array list get all to equal [3, 5, 8]", () => {
+    it("It should get all to equal [3, 5, 8]", () => {
         array.add(3);
         array.add(5);
         array.add(8);
         expect(array.getAll()).toEqual([3, 5, 8]);
     });
 
-    it("It should array list get length to be 3", () => {
+    it("It should get length to be 3", () => {
         array.add(1);
         array.add(1);
         array.add(2);
         expect(array.getLength()).toBe(3);
     });
 
-    it("It should array list get length to be 0", () => {
+    it("It should get length to be 0", () => {
         expect(array.getLength()).toBe(0);
     });
 });
